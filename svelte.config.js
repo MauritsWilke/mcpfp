d0ded022cfa7f154b0a9908a36f51b337b380cbc
Maurits Wilke <mauritswilke@gmail.com>
2022-03-06 19:55:38 +0100

    build(deployment): :rocket: now using the nodejs adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ out: 'build' }),
 	}
 };
 
