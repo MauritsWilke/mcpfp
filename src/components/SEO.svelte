f532924b5032465579997ddde5edea4e546050dd
Maurits Wilke <mauritswilke@gmail.com>
2022-03-07 20:27:32 +0100

    fix(component): :bug: seo component
    forgot to add this, oops

---
+++ src/components/SEO.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let title = "minecraftpfp.com";
+    export let description = "minecraftpfp.com";
+    export let image = "";
+</script>
+
+<svelte:head>
+    <meta name="title" content={title} />
+
+    <meta name="description" content={description} />
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:standard" />
+    <meta property="og:image" content={image} />
+</svelte:head>
