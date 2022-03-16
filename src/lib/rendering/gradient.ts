19d49e79a5e12ddb251fde22ef259a8c5ec1792d
Maurits Wilke <mauritswilke@gmail.com>
2022-03-16 15:46:38 +0100

    fix(api): :bug: custom gradients now working

--- src/lib/rendering/gradient.ts
+++ src/lib/rendering/gradient.ts
@@ -1,13 +1,16 @@
 
 function changeGradient(ctx, colours: string[] = ["#00cdac", "#02aab0"]) {
-	const gradient = ctx.createLinearGradient(0, 15, 0, 0);
+	colours ??= ["#00cdac", "#02aab0"];
+	const gradient = ctx.createLinearGradient(0, 18.75, 0, 0);
+
 	let interval = 1;
+	const decrement = 1 / (colours.length - 1);
 	colours.forEach(colour => {
 		gradient.addColorStop(interval, colour);
-		interval -= 1 / (colours.length - 1);
+		interval -= decrement;
 	})
 	ctx.fillStyle = gradient;
-	ctx.fillRect(0, 0, 20, 20);
+	ctx.fillRect(0, 0, 18.75, 18.75);
 }
 
 export default changeGradient;
--- src/routes/api/pfp/[username].png.ts
+++ src/routes/api/pfp/[username].png.ts
@@ -17,16 +17,18 @@ export async function get({ params, url }) {
 	}
 
 	try {
-		const searchParams = url.searchParams;
-		const gradient = searchParams.get("gradient")?.split("-")?.forEach(colour => colour = `#${colour}`);
 		const username = params.username;
+
+		const searchParams = url.searchParams;
+		const gradient = searchParams.get("gradient");
+		const colours = gradient ? gradient.split("-").filter(v => v !== '').map(colour => `#${colour}`) : null;
 
 		const canvas = new Canvas(300, 300);
 		const ctx = canvas.getContext("2d");
 		ctx.scale(16, 16)
 		ctx.imageSmoothingEnabled = false;
 
-		changeGradient(ctx, gradient)
+		changeGradient(ctx, colours)
 		await generatePfp(username, ctx);
 
 		const dataURL = await canvas.png;
@@ -50,28 +52,4 @@ export async function get({ params, url }) {
 			}
 		}
 	}
-
 }
-
-// async function getSkin(username: string): Promise<string | never> {
-// 	if (!valid(username)) return Promise.reject(`${username} is an invalid username`);
-// 	const UUID = await getUUID(username);
-// 	const response = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${UUID}`);
-// 	if (!response.ok) return Promise.reject(`Response returned statuscode ${response.status}`);
-// 	const json = await response.json();
-// 	const r = JSON.parse(atob(json.properties[0].value));
-// 	return r.textures.SKIN.url;
-// }
-
-// async function getUUID(username: string): Promise<string | never> {
-// 	if (!valid(username)) return Promise.reject(`${username} is an invalid username`);
-// 	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
-// 	if (!response.ok) return Promise.reject(`${username} does not exist`);
-// 	const json = await response.json();
-// 	return json.id;
-// }
-
-// function valid(username: string) {
-// 	if (!username.match(/^[a-z0-9_]*$/i)) return false;
-// 	return true;
-// }
