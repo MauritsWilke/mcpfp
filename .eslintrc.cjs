64cd0a8e076bc5ee8c0fee03baf6d8b4b4d0e9ee
Maurits Wilke <mauritswilke@gmail.com>
2022-08-10 17:34:16 +0200

    feat: :wrench: ESLint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,41 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		// Semicolons
+		"semi": "error",
+		"semi-style": ["error", "last"],
+
+		// Spaces / Tabs
+		"indent": ["error", "tab"],
+		"no-trailing-spaces": "error",
+
+		// Comments
+		"multiline-comment-style": ["error", "starred-block"],
+		"spaced-comment": "error",
+
+		// Variables
+		"no-var": "error",
+		"prefer-const": "error",
+
+		// Logic
+		"no-self-compare": "warn",
+		"eqeqeq": "error",
+
+		// Mistakes
+		"no-template-curly-in-string": "error",
+
+		// Formatting
+		"dot-notation": "error",
+		"array-bracket-newline": ["error", { "multiline": true, "minItems": 2 }],
+		"multiline-ternary": ["error", "always-multiline"],
+		"arrow-spacing": [
+			"error",
+			{
+				"before": true,
+				"after": true
+			}
+		],
 	}
 };
