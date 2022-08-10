9dbfb89c3c5d312ef6e441e30e7e1ac2f05b4408
Maurits Wilke <mauritswilke@gmail.com>
2022-08-10 16:54:01 +0200

    style: :art: formatted code using prettier

--- src/lib/components/placeholder.svelte
+++ src/lib/components/placeholder.svelte
@@ -1,3 +1,3 @@
 <script lang="ts">
-	import placeholder from '@scripts/placeholder';
+	import placeholder from "@scripts/placeholder";
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
 import path from "path";
 
 /** @type {import('vite').UserConfig} */
